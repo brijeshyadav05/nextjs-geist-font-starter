0473d0a70513282a1b48036ee8d3faa6b56ac3d9
brijeshyadav05 <by936743@gmail.com>
2025-07-19 22:36:39 +0530

    Add .next/static/chunks/src_app_profile_page_tsx_569d93c9._.js 1752944802771

---
+++ .next/static/chunks/src_app_profile_page_tsx_569d93c9._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_dc773f73._.js",
+  "static/chunks/src_08c02c9e._.js"
+],
+    source: "dynamic"
+});
